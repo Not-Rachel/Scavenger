b1e70fc77ec83f0c640fe7ee8474424f46d0a48b
Rachel Brinkman <98926561+Not-Rachel@users.noreply.github.com>
2025-11-17 18:17:35 -0800

    Restyle homepage

--- src/landing/Home.tsx
+++ src/landing/Home.tsx
@@ -1,6 +1,6 @@
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import portrait from "../assets/rache2.jpg";
+import portrait from "../assets/dither-bw.png";
 import construction from "../assets/construction.gif";
 import { ReactSVG } from "react-svg";
 import scavLogo from "../assets/logo2.svg";
@@ -9,30 +9,39 @@ import Cubes from "../pages/Cubes";
 function Home() {
   const navigate = useNavigate();
   const container =
-    "border-1 border-white flex justify-center border-dashed p-2 w-32 h-32";
+    "border-1 border-white flex justify-center items-center border-dashed  w-24 md:w-48 md:h-48 sm:w-36 sm:h-36 h-24";
   return (
-    <div className="flex justify-center bg-radial-[at_15%_50%] from-yellow-400 via-blue-400 to-blue-950 to-100%">
-      <div className="self-center bg-black text-3xl font-serif text-white h-[200vh] max-w-[500px] p-4 flex items-center flex-col  [box-shadow:inset_0_0_1500px_rgba(30,35,55,0.7)]">
-        <div className="flex flex-row gap-8 justify-center items-center mt-8">
+    <div className="flex justify-center bg-radial-[at_15%_50%] from-gray-700  to-black to-100% font-[bleguk]">
+      <div className="  bg-black text-4xl  text-white h-[200vh] max-w-[700px] p-4 flex items-center flex-col  ">
+        <div
+          className="absolute inset-0 pointer-events-none z-10"
+          style={{
+            boxShadow: "inset 0 0 1500px rgba(30,35,55,0.7)",
+          }}
+        />
+
+        <div className="w-full  flex flex-row justify-center  ">
+          <div className=" pt-16 ">
+            <h1 className="text-nowrap pb-16">Rachel Brinkman</h1>
+            <p className="text-2xl">I love to design creative websites</p>
+            <p className="text-2xl">
+              {" "}
+              Please check out any of the projects below
+            </p>
+          </div>
           <img
             src={portrait}
             alt="Portrait"
-            className="border-2 border-dotted border-white w-48 h-48 object-cover"
+            className="w-64 h-auto object-cover"
           />
-          <div className="p-2">
-            <h1 className="">Rachel Brinkman</h1>
-            <div className="flex flex-row items-center gap-4 my-4">
-              <a href="https://github.com/Not-Rachel">
-                <ReactSVG src={github} className="text-blue-200 w-12" />
-              </a>
-              <a href="">LinkedIn</a>
-            </div>
-          </div>
         </div>
         <h3 className="mt-8 w-full">Projects</h3>
-        <div className="grid grid-cols-3 gap-12 my-8">
+        <div className="w-full grid grid-cols-3 gap-12 my-8">
           <a href="/scavenger" target="_blank" className={container}>
-            <ReactSVG src={scavLogo} className="text-amber-50 w-21" />
+            <ReactSVG
+              src={scavLogo}
+              className="text-amber-50 w-16 sm:24 md:w-32"
+            />
           </a>
           <a
             href="../../../Graphics 2024/A7/project7.html"
@@ -42,21 +51,26 @@ function Home() {
             <Cubes></Cubes>
           </a>
           <a href=".." target="_blank" className={container}>
-            <ReactSVG src={scavLogo} className="text-amber-50 w-21" />
+            <h1 className="text-amber-50 text-xl  w-24 md:w-32 font-[EdSloppy] text-center">
+              ScavHunt
+            </h1>
           </a>
-          <a href="/scavenger" target="_blank" className={container}>
-            <img src={construction} alt="Under construction" />
+          <a href=".." target="_blank" className={container}>
+            <h1 className="text-amber-50 text-3xl  w-24 md:w-32 font-[Daubmark] text-center">
+              Watch for Deer
+            </h1>
           </a>
-          <a
-            href="https://not-rachel.info/scavenger"
-            target="_blank"
-            className={container}
-          >
-            <img src={construction} alt="Under construction" />
+          <a href=".." target="_blank" className={container}>
+            <h1 className="text-amber-50 text-3xl  w-24 md:w-32 font-[Daubmark] text-center">
+              Offline
+            </h1>
           </a>
-          <a href="/" target="_blank" className={container}>
-            <img src={construction} alt="Under construction" />
+          <a href=".." target="_blank" className={container}>
+            <h1 className="text-amber-50 text-xl  w-24 md:w-32 font-[EdSloppy] text-center">
+              Dither Punk
+            </h1>
           </a>
+
           {/* <a onClick={() => navigate("/cubes")}>WebGL Demo</a> */}
         </div>
       </div>
